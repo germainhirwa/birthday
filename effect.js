8c35dd1a6f5015732f4ee05c21bb14263db4c4cb
Germain Hirwa <higermain1@gmail.com>
2025-11-18 17:34:56 -0500

    Nela birthday wish

--- effect.js
+++ effect.js
@@ -2,55 +2,23 @@ $(window).load(function(){
 	$('.loading').fadeOut('fast');
 	$('.container').fadeIn('fast');
 });
+
 $('document').ready(function(){
-		var vw;
-		$(window).resize(function(){
-			 vw = $(window).width()/2;
-			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
-			$('#b11').animate({top:240, left: vw-350},500);
-			$('#b22').animate({top:240, left: vw-250},500);
-			$('#b33').animate({top:240, left: vw-150},500);
-			$('#b44').animate({top:240, left: vw-50},500);
-			$('#b55').animate({top:240, left: vw+50},500);
-			$('#b66').animate({top:240, left: vw+150},500);
-			$('#b77').animate({top:240, left: vw+250},500);
-		});
-
-	$('#turn_on').click(function(){
-		$('#bulb_yellow').addClass('bulb-glow-yellow');
-		$('#bulb_red').addClass('bulb-glow-red');
-		$('#bulb_blue').addClass('bulb-glow-blue');
-		$('#bulb_green').addClass('bulb-glow-green');
-		$('#bulb_pink').addClass('bulb-glow-pink');
-		$('#bulb_orange').addClass('bulb-glow-orange');
-		$('body').addClass('peach');
-		$(this).fadeOut('slow').delay(5000).promise().done(function(){
-			$('#play').fadeIn('slow');
-		});
-	});
-	$('#play').click(function(){
-		var audio = $('.song')[0];
-        audio.play();
-        $('#bulb_yellow').addClass('bulb-glow-yellow-after');
-		$('#bulb_red').addClass('bulb-glow-red-after');
-		$('#bulb_blue').addClass('bulb-glow-blue-after');
-		$('#bulb_green').addClass('bulb-glow-green-after');
-		$('#bulb_pink').addClass('bulb-glow-pink-after');
-		$('#bulb_orange').addClass('bulb-glow-orange-after');
-		$('body').css('backgroud-color','#FFF');
-		$('body').addClass('peach-after');
-		$(this).fadeOut('slow').delay(6000).promise().done(function(){
-			$('#bannar_coming').fadeIn('slow');
-		});
+	var vw;
+	
+	$(window).resize(function(){
+		vw = $(window).width()/2;
+		$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
+		$('#b11').animate({top:240, left: vw-350},500);
+		$('#b22').animate({top:240, left: vw-250},500);
+		$('#b33').animate({top:240, left: vw-150},500);
+		$('#b44').animate({top:240, left: vw-50},500);
+		$('#b55').animate({top:240, left: vw+50},500);
+		$('#b66').animate({top:240, left: vw+150},500);
+		$('#b77').animate({top:240, left: vw+250},500);
 	});
 
-	$('#bannar_coming').click(function(){
-		$('.bannar').addClass('bannar-come');
-		$(this).fadeOut('slow').delay(6000).promise().done(function(){
-			$('#balloons_flying').fadeIn('slow');
-		});
-	});
-
+	// Balloon animation functions
 	function loopOne() {
 		var randleft = 1000*Math.random();
 		var randtop = 500*Math.random();
@@ -58,6 +26,7 @@ $('document').ready(function(){
 			loopOne();
 		});
 	}
+	
 	function loopTwo() {
 		var randleft = 1000*Math.random();
 		var randtop = 500*Math.random();
@@ -65,6 +34,7 @@ $('document').ready(function(){
 			loopTwo();
 		});
 	}
+	
 	function loopThree() {
 		var randleft = 1000*Math.random();
 		var randtop = 500*Math.random();
@@ -72,6 +42,7 @@ $('document').ready(function(){
 			loopThree();
 		});
 	}
+	
 	function loopFour() {
 		var randleft = 1000*Math.random();
 		var randtop = 500*Math.random();
@@ -79,6 +50,7 @@ $('document').ready(function(){
 			loopFour();
 		});
 	}
+	
 	function loopFive() {
 		var randleft = 1000*Math.random();
 		var randtop = 500*Math.random();
@@ -94,6 +66,7 @@ $('document').ready(function(){
 			loopSix();
 		});
 	}
+	
 	function loopSeven() {
 		var randleft = 1000*Math.random();
 		var randtop = 500*Math.random();
@@ -102,54 +75,74 @@ $('document').ready(function(){
 		});
 	}
 
-	$('#balloons_flying').click(function(){
-		$('.balloon-border').animate({top:-500},8000);
-		$('#b1,#b4,#b5,#b7').addClass('balloons-rotate-behaviour-one');
-		$('#b2,#b3,#b6').addClass('balloons-rotate-behaviour-two');
-		// $('#b3').addClass('balloons-rotate-behaviour-two');
-		// $('#b4').addClass('balloons-rotate-behaviour-one');
-		// $('#b5').addClass('balloons-rotate-behaviour-one');
-		// $('#b6').addClass('balloons-rotate-behaviour-two');
-		// $('#b7').addClass('balloons-rotate-behaviour-one');
-		loopOne();
-		loopTwo();
-		loopThree();
-		loopFour();
-		loopFive();
-		loopSix();
-		loopSeven();
+	// MAIN CELEBRATION SEQUENCE
+	$('#turn_on').click(function(){
+		$(this).fadeOut('slow');
 		
-		$(this).fadeOut('slow').delay(5000).promise().done(function(){
-			$('#cake_fadein').fadeIn('slow');
-		});
-	});	
-
-	$('#cake_fadein').click(function(){
-		$('.cake').fadeIn('slow');
-		$(this).fadeOut('slow').delay(3000).promise().done(function(){
-			$('#light_candle').fadeIn('slow');
-		});
-	});
-
-	$('#light_candle').click(function(){
-		$('.fuego').fadeIn('slow');
-		$(this).fadeOut('slow').promise().done(function(){
+		// Step 1: Turn on lights
+		$('#bulb_yellow').addClass('bulb-glow-yellow');
+		$('#bulb_red').addClass('bulb-glow-red');
+		$('#bulb_blue').addClass('bulb-glow-blue');
+		$('#bulb_green').addClass('bulb-glow-green');
+		$('#bulb_pink').addClass('bulb-glow-pink');
+		$('#bulb_orange').addClass('bulb-glow-orange');
+		$('body').addClass('peach');
+		
+		// Step 2: Start music immediately
+		setTimeout(function(){
+			var audio = $('.song')[0];
+			audio.play();
+			$('#bulb_yellow').addClass('bulb-glow-yellow-after');
+			$('#bulb_red').addClass('bulb-glow-red-after');
+			$('#bulb_blue').addClass('bulb-glow-blue-after');
+			$('#bulb_green').addClass('bulb-glow-green-after');
+			$('#bulb_pink').addClass('bulb-glow-pink-after');
+			$('#bulb_orange').addClass('bulb-glow-orange-after');
+			$('body').addClass('peach-after');
+		}, 1000);
+		
+		// Step 3: Show banner (decoration)
+		setTimeout(function(){
+			$('.bannar').addClass('bannar-come');
+		}, 2000);
+		
+		// Step 4: Fly balloons (2 seconds after banner starts)
+		setTimeout(function(){
+			$('.balloon-border').animate({top:-500},8000);
+			$('#b1,#b4,#b5,#b7').addClass('balloons-rotate-behaviour-one');
+			$('#b2,#b3,#b6').addClass('balloons-rotate-behaviour-two');
+			loopOne();
+			loopTwo();
+			loopThree();
+			loopFour();
+			loopFive();
+			loopSix();
+			loopSeven();
+		}, 4000);
+		
+		// Step 5: Show cake (5 seconds after balloons start)
+		setTimeout(function(){
+			$('.cake').fadeIn('slow');
+		}, 9000);
+		
+		// Step 6: Show "Happy Birthday Nela" button
+		setTimeout(function(){
 			$('#wish_message').fadeIn('slow');
-		});
+		}, 12000);
 	});
 
-		
+	// Happy Birthday Nela button - arranges balloons to spell NELA
 	$('#wish_message').click(function(){
-		 vw = $(window).width()/2;
+		vw = $(window).width()/2;
 
 		$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
 		$('#b1').attr('id','b11');
-		$('#b2').attr('id','b22')
-		$('#b3').attr('id','b33')
-		$('#b4').attr('id','b44')
-		$('#b5').attr('id','b55')
-		$('#b6').attr('id','b66')
-		$('#b7').attr('id','b77')
+		$('#b2').attr('id','b22');
+		$('#b3').attr('id','b33');
+		$('#b4').attr('id','b44');
+		$('#b5').attr('id','b55');
+		$('#b6').attr('id','b66');
+		$('#b7').attr('id','b77');
 		$('#b11').animate({top:240, left: vw-350},500);
 		$('#b22').animate({top:240, left: vw-250},500);
 		$('#b33').animate({top:240, left: vw-150},500);
@@ -159,11 +152,13 @@ $('document').ready(function(){
 		$('#b77').animate({top:240, left: vw+250},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
+		
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
 			$('#story').fadeIn('slow');
 		});
 	});
 	
+	// Message button - shows the birthday message
 	$('#story').click(function(){
 		$(this).fadeOut('slow');
 		$('.cake').fadeOut('fast').promise().done(function(){
@@ -174,28 +169,19 @@ $('document').ready(function(){
 
 		function msgLoop (i) {
 			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
-			i=i+1;
-			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
-			if(i==50){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
-					$('.cake').fadeIn('fast');
-				});
-				
-			}
-			else{
-				msgLoop(i);
-			}			
-
-		});
-			// body...
+				i=i+1;
+				$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
+				if(i==48){
+					$("p:nth-child("+i+")").fadeOut('slow').delay(2000).promise().done(function () {
+						$('.cake').fadeIn('fast');
+					});
+				}
+				else{
+					msgLoop(i);
+				}			
+			});
 		}
 		
 		msgLoop(0);
-		
 	});
 });
-
-
-
-
-//alert('hello');
